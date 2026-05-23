14bb36e4161414d7a71d1657f047f877a564bc30
Carlos Barba Herrador <carlillosbarba@gmail.com>
2026-05-23 05:07:38 +0200

    add main.py entry point

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from repl-nix-workspace!")
+import os
+import sys
 
+os.chdir(os.path.join(os.path.dirname(__file__), "discord-bot"))
+sys.path.insert(0, os.getcwd())
+
+from bot import main
 
 if __name__ == "__main__":
     main()
